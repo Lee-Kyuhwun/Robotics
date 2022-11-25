f0d439def340baccfc8a0e2a47def70e020bebcf
Lee-KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2022-11-25 13:42:55 +0900

    Delete 팀플 directory

--- 팀플/draw_line.m
+++
@@ -1,3 +0,0 @@
-function draw_line(p1, p2, lw, clr)
-line([p1(1,1),p2(1,1)],[p1(2,1),p2(2,1)],[p1(3,1),p2(3,1)],'LineWidth',lw,'Color',clr);
-end
--- 팀플/tinh_toadoE.m
+++
@@ -1,5 +0,0 @@
-function toado_tdc = tinh_toadoE(theta1,theta2,d3,a1,a2)
-
-d2r = pi/180;
-toado_tdc = [a2*cosd(theta1+theta2)+a1*cosd(theta1);a2*sind(theta1+theta2)+a1*sind(theta1);-d3]';
-end
